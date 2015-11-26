3196e7f653e9d4561917a0e69f372948128e49d1
Noel <harrisbnoel@gmail.com>
2015-11-25 22:45:04 -0500

    Final product for grading

---
+++ DoroPortfolioWebsite/Controllers/ProjectsController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DoroPortfolioWebsite.Models;
+
+namespace DoroPortfolioWebsite.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private ProjectDatabaseEntities db = new ProjectDatabaseEntities();
+        // GET: Projects
+        public ActionResult Index()
+        {
+            return View(db.Projects.ToList());
+        }
+
+        public ActionResult EditableIndex()
+        {
+            return View(db.Projects.ToList());
+        }
+
+        // GET: Projects/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Project/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProjectID,Project_Name,Description,Image,GitHub_Repo")] Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Projects.Add(project);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(project);
+        }
+
+        // GET: Movies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project project = db.Projects.Find(id);
+            if (project == null)
+            {
+                return HttpNotFound();
+            }
+            return View(project);
+        }
+
+        // POST: Projects/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProjectID,Project_Name,Description,Image,GitHub_Repo")] Project project)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(project).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(project);
+        }
+
+        // GET: Projects/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Project movie = db.Projects.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Projects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Project movie = db.Projects.Find(id);
+            db.Projects.Remove(movie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DoroPortfolioWebsite/Views/Shared/ContactMe.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title></title>
+</head>
+<body>
+<?php
+// check if fields passed are empty
+// php file that I found to help me get my contact form together
+// Still learning php so the tutorial was helpful
+// Needed to download PHP tools free trial to get VS to read my php file
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+	
+// create email body and send it	
+$to = 'dhunt3@kent.edu'; // put your email
+$email_subject = "Contact form submitted by:  $name";
+$email_body = "You have received a new message. \n\n".
+				  " Here are the details:\n \nName: $name \n ".
+				  "Email: $email_address\n Message \n $message";
+$headers = "From: dhunt3@kent.edu\n";
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
+</body>
+</html>
